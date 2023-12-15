32bc50f4241fc0635daec7d19c85b9c24c01e427
bilal <kharbachbilale@gmail.com>
2023-12-15 06:26:31 -0500

    premier pas

---
+++ src/test/java/com/bilal/calculator/CalculatorTest.java
@@ -0,0 +1,11 @@
+package com.bilal.calculator;
+import org.junit.Test;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class CalculatorTest {
+	private Calculator calculator = new Calculator();
+	@Test
+	public void testSum() {
+	assertEquals(8, calculator.sum(3, 5));
+	}
+      }
